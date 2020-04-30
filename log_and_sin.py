6c665c8ebdc98bbff563707b96871d3f52339942
mewes4 <mewes4@web.de>
2020-04-30 17:35:37 +0200

    added docstrings

--- log_and_sin.py
+++ log_and_sin.py
@@ -3,24 +3,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def faculty(N):
+	"""
+	this function calculates and returns the falculty for argument N
+	"""
 	res = 1
 	for n in range(N):
 		res *= (n + 1)
 	return res
 
 def sin_series(x,N):
-	res = x
+	"""
+	compute the value of sinus using its series.
+	x: the argument of sin(x)
+	N: how many terms of the series should be computed
+	"""
+	res = x #value of the first term of the series
 	for n in range(1,N+1):
 		res = res + (-1)**n/faculty(2*n+1)*x**(2*n+1)
 	return res
 
 def log_series(x,N):
-	res = x-1
+	"""
+	compute the value of the logarithm using its series.
+	x: the argument of log(x)
+	N: how many terms of the series should be computed
+	"""
+	res = x-1 #value of the first term of the series
 	for n in range(2,N+1):
 		res = res + (-1)**(n+1)/n*(x-1)**n
 	return res
 
 def main():
+	"""
+	the difference between the series value and the exact value 
+	according to math module are compared for different numbers of 
+	terms N. This change of error is portrayed using matplotlib.
+	"""
 	x=1
 	error=[]
 	N_values= np.arange(1,9)
